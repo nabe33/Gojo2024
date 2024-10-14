c3b33b8d21d9a78952a962e093f41dedd3d68856
nabe <nabe@lab.twcu.ac.jp>
2024-10-15 08:50:31 +0900

    ヘルプカードが拡大表示されているように修正

--- lib/helpCard/enlarge_helpcard.dart
+++ lib/helpCard/enlarge_helpcard.dart
@@ -17,40 +17,48 @@ class EnlargeHelpCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Center(
-            //   child: Text(
-            //     'ご協力お願い',
-            //     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            //   ),
-            // ),
-            // SizedBox(height: 20),
-            Center(
-              child: Text(
-                '私は認知症です',
-                style: TextStyle(fontSize: 24, color: Colors.black),
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
               ),
-            ),
-            SizedBox(height: 8),
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(8),
+              shadowColor: Colors.orange,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        '私は認知症です',
+                        style: TextStyle(fontSize: 24, color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.all(4),
+                        child: Text(
+                          helpCard['contents'] ?? 'No Contents',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Center(
+                      child: Text(
+                        'お手伝いしてください',
+                        style: TextStyle(fontSize: 24, color: Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  helpCard['contents'] ?? 'No Contents',
-                  style: TextStyle(fontSize: 24),
-                ),
               ),
             ),
-            SizedBox(height: 8),
-            Center(
-              child: Text(
-                'お手伝いしてください',
-                style: TextStyle(fontSize: 24, color: Colors.black),
-              ),
-            ),
+            //
             SizedBox(height: 20),
             Center(
               child: ElevatedButton(
@@ -64,7 +72,7 @@ class EnlargeHelpCard extends StatelessWidget {
                   //     context, true); // Return true to indicate completion
                 },
                 style: ElevatedButton.styleFrom(
-                    elevation: 4, backgroundColor: Colors.orange),
+                    elevation: 4, backgroundColor: Color(0xff82d3e3)),
                 child: Text(
                   'サポート方法のヒント',
                   style: TextStyle(
